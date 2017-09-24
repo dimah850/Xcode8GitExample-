984b8caeccadc8784c966e9e518ab85be11c23fe
iRepair <irepair@iRepairs-iMac.local>
2017-09-24 13:44:22 +0330

    Initial Commit

---
+++ Xcode8GitExaple/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8GitExaple
+//
+//  Created by iRepair on 9/24/17.
+//  Copyright © 2017 iRepair. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
